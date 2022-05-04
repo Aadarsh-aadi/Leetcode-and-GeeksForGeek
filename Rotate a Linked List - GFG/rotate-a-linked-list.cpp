d1eb8521d6164f278664e60a94e5b1b2b76bf955
Aadarsh-aadi <78400814+Aadarsh-aadi@users.noreply.github.com>
2022-05-04 13:40:42 +0530

    Added solution - LeetHub

---
+++ Rotate a Linked List - GFG/rotate-a-linked-list.cpp
@@ -0,0 +1,117 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+        if(head == NULL)
+        {
+            return NULL;
+        }
+        if(head->next == NULL)
+        {
+            return head;
+        }
+        
+        Node* curr = head;
+        Node* tail = NULL;
+        Node* begin = NULL;
+        
+        k--;
+        
+        while(curr->next != NULL)
+        {
+            if(k == 0)
+            {
+                tail = curr;
+            }
+            curr = curr->next;
+            k--;
+        }
+        
+        if(k == 0)
+        {
+            return head;
+        }
+        
+        begin = tail->next;
+        tail->next = NULL;
+        curr->next = head;
+        
+        return begin;
+    }
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        Solution ob;
+        head = ob.rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+  // } Driver Code Ends
