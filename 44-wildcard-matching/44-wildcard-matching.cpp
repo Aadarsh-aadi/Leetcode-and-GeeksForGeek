69621bafc6294de14fa01edb3f14511c88faa884
Aadarsh-aadi <78400814+Aadarsh-aadi@users.noreply.github.com>
2022-05-22 14:11:22 +0530

    Time: 58 ms (65.11%), Space: 7.6 MB (81.12%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    bool isMatch(string A, string B) 
+    bool isMatch(string s, string p) 
     {
-           int n = A.size() , m = B.size() , i , j;
-    bool dp[n+1][m+1];
-    memset(dp, false, sizeof(dp));
-    
-    dp[0][0]= true;
-
-    for(j=1;j<=m;++j)
-    {
-        dp[0][j] = (B[j-1]=='*') && (dp[0][j-1]);
-    }
-    dp[0][0] = true;
-
-    for(i=1;i<=n;++i)
-    {
-        for(j=1;j<=m;++j)
+        int n = s.size() , m = p.size() , i ,j;
+        bool dp[n+1][m+1];
+        memset(dp,false,sizeof(dp));
+        dp[0][0] = true;
+        
+        for(i=1;i<=m;++i)
+            dp[0][i] = (dp[0][i-1]) && (p[i-1] == '*');
+        
+        for(i=1;i<=n;++i)
         {
-            if(B[j-1] == '?' || B[j-1] == A[i-1] )
-            dp[i][j] = dp[i-1][j-1];
-
-            else if(B[j-1] == '*')
-            dp[i][j] = dp[i][j-1] || dp[i-1][j];
-
-            else
-            dp[i][j] = false;
+            for(j=1;j<=m;++j)
+            {
+                if(p[j-1] == '?' || p[j-1] == s[i-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else
+                    dp[i][j] = false;
+            }
         }
-    }
-
-    return dp[n][m];
+        
+        return dp[n][m];
     }
 };
