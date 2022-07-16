0a15ec476b3c2fdbbc1fc2c1a5d83c0114da8b66
Aadarsh-aadi <78400814+Aadarsh-aadi@users.noreply.github.com>
2022-07-16 14:44:01 +0530

    Time: 185 ms (73.25%), Space: 9 MB (91.30%) - LeetHub

---
+++ 1449-form-largest-integer-with-digits-that-add-up-to-target/1449-form-largest-integer-with-digits-that-add-up-to-target.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& cost, int target) 
+    {
+        vector<int> maxDigit(target+1,INT_MIN);
+        int i , j ;
+        maxDigit[0] = 0;
+        for(i = 1 ; i<=target ;++i)
+        {
+            for(j = 0 ; j <= 8 ; ++j)
+            {
+                if(cost[j] <= i)
+                    maxDigit[i] = max(maxDigit[i] , maxDigit[i-cost[j]] + 1);
+                    
+            }
+            
+            cout << maxDigit[i] << " "<<i << endl;
+        }
+        
+        if(maxDigit[target] < 0)
+            return "0";
+        
+        string ans;
+        
+        for(i = 8 ; i >= 0 ; --i)
+        {
+            while(target >= cost[i] && maxDigit[target] == maxDigit[target - cost[i]] + 1)
+            {
+                target -= cost[i];
+                ans += (char)('0' + (i+1));
+            }
+        }
+        
+        return ans;
+        
+    }
+};
