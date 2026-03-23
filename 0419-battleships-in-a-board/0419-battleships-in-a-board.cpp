b46f788f9e2f9b1a2179efd1e617807a7e4cca0c
Aadarsh-aadi <78400814+Aadarsh-aadi@users.noreply.github.com>
2026-03-23 10:47:26 +0530

    Time: 0 ms (100%), Space: 11.9 MB (96.83%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int n = board.size(), m = board[0].size();
+        int ans = 0 , i , j;
+        for(i=0; i < n ; ++i){
+            for(j=0;j<m;++j){
+                if(board[i][j]=='X'){
+                    if(i+1 < n && board[i+1][j]=='X'){}
+                    else if(j+1 < m && board[i][j+1]=='X'){
+                        while(j+1 < m && board[i][j+1]=='X'){
+                            j++;
+                        }
+                        ans++;
+                    } else{
+                        ans++;
+                    }
+                }
+            }
+        }
+
+        // for(j=0;j<m;++j){
+        //     for(i=0;i<n;++i){
+        //         if(board[i][j]=='X'){
+        //             if(i+1 < n && board[i+1][j] == 'X'){
+        //                 while(i+1 < n && board[i+1][j] == 'X'){
+        //                     i++;
+        //                 }
+        //                 if(i == n-1 && board[i][j] == 'X'){}
+        //                 else
+        //                 ans++;
+        //             }
+        //         }
+        //     }
+        // }
+
+        return ans;
+    }
+};
